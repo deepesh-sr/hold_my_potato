558b1ccdc42f8e31ea9aff7de569a6e39e4cc7e1
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-06-12 22:26:41 +0530

    basic code setup for learning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use bevy::{prelude::*, state::commands};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_plugins(HelloPlugin)
+    .run();
+
+}
+fn hello_world() {
+    println!("hello world!");
+}
+
+#[derive(Component)]
+struct Person;
+
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people ( mut commands : Commands){
+    commands.spawn((Person, Name("Deepesh".to_string())));
+    commands.spawn((Person, Name("Shivam".to_string())));
+    commands.spawn((Person, Name("Shrijan".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+    app.add_systems(Startup, add_people);
+    app.add_systems(Update, (hello_world,greet_people));
+    }
 }
